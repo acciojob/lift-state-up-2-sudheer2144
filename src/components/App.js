92cfe909bf59fa41f0013be1d1c3b8439ba1e2c0
sudheer2144 <sudheer.ged@gmail.com>
2023-08-18 22:53:50 +0530

    submitting code-Fri Aug 18 2023 22:53:48 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [txt, setTxt] = useState("")
+
+  function displayText(event) {
+    setTxt(event.target.value)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      {txt ? <p>{txt}</p> : null}
+      <div className="child">
+        <h2>Child Component</h2>
+        <input type="text" name="" id="" onChange={displayText} />
+      </div>
     </div>
   )
 }
